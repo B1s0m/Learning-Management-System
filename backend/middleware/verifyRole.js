2c1f79f20a06dea4b7d36dcd868a3bf8dd2050e9
B1s0m <b0b.30916@gmail.com>
2026-08-10 11:30:54 +0300

    add verifyInstructorOrAdimin and verifyStudent

--- backend/middleware/verifyRole.js
+++ backend/middleware/verifyRole.js
@@ -48,8 +48,57 @@ async function verifyInstructor(req, res,next) {
     })
   }
 }
+
+async function verifyStudent(req, res,next) {
+  try {
+    const user = await User.findById(req.user._id)
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found."
+      })
+    }else if(user.role !== "student"){
+        return res.status(403).json({
+          message: "You are not authorized to do this."
+        })
+    }
+      next()
+
+  } catch (err) {
+    console.error(err)
+
+    return res.status(500).json({
+      message: "Internal Server Error"
+    })
+  }
+}
+
+async function verifyInstructorOrAdimin(req, res,next) {
+  try {
+    const user = await User.findById(req.user._id)
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found."
+      })
+    }else if(user.role == "student"){
+        return res.status(403).json({
+          message: "You are not authorized to do this."
+        })
+    }
+      next()
+
+  } catch (err) {
+    console.error(err)
+
+    return res.status(500).json({
+      message: "Internal Server Error"
+    })
+  }
+}
 module.exports={
 verifyAdmin,
-verifyInstructor
+verifyInstructor ,
+verifyInstructorOrAdimin ,verifyStudent
 
 }
